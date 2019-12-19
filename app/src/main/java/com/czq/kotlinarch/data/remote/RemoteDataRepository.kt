da2db9fa9035a0e304f54f7f498dff9c36a8389d
manondidi <790512128@qq.com>
2019-12-19 12:25:55 +0800

    更新依赖关系

--- app/src/main/java/com/czq/kotlinarch/data/remote/RemoteDataRepository.kt
+++ app/src/main/java/com/czq/kotlinarch/data/remote/RemoteDataRepository.kt
@@ -6,6 +6,7 @@ import com.czq.kotlinarch.data.remote.api.RemoteApi
 import io.reactivex.Observable
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.fastjson.FastJsonConverterFactory
