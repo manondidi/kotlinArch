ccae6ac9672c73b0bf467bff35b3b6dc3897d68c
manondidi <790512128@qq.com>
2019-12-30 17:14:07 +0800

    服务器快过期了, 把数据从远程拉取 变成加载本地json文件

--- app/src/main/java/com/czq/kotlinarch/data/remote/MockDataRepository.kt
+++ app/src/main/java/com/czq/kotlinarch/data/remote/MockDataRepository.kt
@@ -3,10 +3,7 @@ package com.czq.kotlinarch.data.remote
 import com.alibaba.fastjson.TypeReference
 import com.czq.kotlin_arch.common.util.MockUtil
 import com.czq.kotlinarch.App
-import com.czq.kotlinarch.data.model.Banner
-import com.czq.kotlinarch.data.model.ChallengeRecomand
-import com.czq.kotlinarch.data.model.Page
-import com.czq.kotlinarch.data.model.User
+import com.czq.kotlinarch.data.model.*
 import io.reactivex.Observable
 import java.util.concurrent.TimeUnit
 
@@ -19,13 +16,44 @@ class MockDataRepository() {
     }
 
     fun getUser(): Observable<User> {
-        return Observable.just(MockUtil.getMockModel(App.application, "get_user.json", User::class.java))
+        return Observable.just(
+            MockUtil.getMockModel(
+                App.application,
+                "get_user.json",
+                User::class.java
+            )
+        )
             .delay(1, TimeUnit.SECONDS)
     }
 
     fun getBanners(): Observable<List<Banner>> {
-        return Observable.just(MockUtil.getMockModelList(App.application, "banner.json", Banner::class.java))
-                .delay(1, TimeUnit.SECONDS)
+        return Observable.just(
+            MockUtil.getMockModelList(
+                App.application,
+                "banner.json",
+                Banner::class.java
+            )
+        )
+            .delay(1, TimeUnit.SECONDS)
     }
 
+    fun getGames(): Observable<Page<Game>> {
+        return Observable.just(
+            MockUtil.getMockModel(
+                App.application,
+                "game.json",
+                object : TypeReference<Page<Game>>() {})
+        )
+            .delay(1, TimeUnit.SECONDS)
+    }
+
+    fun getFeeds(): Observable<List<FeedArticle>> {
+        return Observable.just(
+            MockUtil.getMockModel(
+                App.application,
+                "feeds.json",
+                object : TypeReference<List<FeedArticle>>() {})
+        )
+            .delay(1, TimeUnit.SECONDS)
+    }
 }
