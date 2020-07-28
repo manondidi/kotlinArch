11393ae88325b5bfc189c5de60d1f6c24198e149
chenzhiqiang <790512128@qq.com>
2020-07-28 13:41:36 +0800

    隐藏导航栏不可恢复

--- kotlin_arch/src/main/java/com/czq/kotlin_arch/common/util/ext/Activity.kt
+++ kotlin_arch/src/main/java/com/czq/kotlin_arch/common/util/ext/Activity.kt
@@ -12,8 +12,7 @@ fun AppCompatActivity.hideBottom() {
         v.systemUiVisibility = View.GONE
     } else if (Build.VERSION.SDK_INT >= 19) { //for new api versions.
         val decorView = window.decorView
-        val uiOptions = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        val uiOptions = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION  or View.SYSTEM_UI_FLAG_FULLSCREEN)
         decorView.systemUiVisibility = uiOptions
     }
 }
